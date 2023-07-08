0974574b8af2fb075c2eee5d4a4bc7556a4aa1f5
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2023-07-08 14:27:32 +0530

    UI part is ready for signup and login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function LoginPage () {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  })
+
+  const onLogin = async () => {
+
+  }
+  
   return (
-    <div className="flex">
-      <h1 className="text-center text-white text-2xl">Login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+      <label htmlFor="email">E-mail</label>
+      <input
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({
+          ...user,
+          email: e.target.value
+        })}
+        placeholder="email"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({
+          ...user,
+          password: e.target.value
+        })}
+        placeholder="password"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" onClick={onLogin}>login Here</button>
+      <Link href="/signup">Visit Signup Page</Link>
     </div>
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,62 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignupPage () {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: ""
+  })
+
+  const onSignup = async () => {
+
+  }
+  
   return (
-    <div className="flex">
-      <h1 className="text-center text-white text-2xl">Signup</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Sign Up</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({
+          ...user,
+          username: e.target.value
+        })}
+        placeholder="username"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="email">E-mail</label>
+      <input
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({
+          ...user,
+          email: e.target.value
+        })}
+        placeholder="email"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({
+          ...user,
+          password: e.target.value
+        })}
+        placeholder="password"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" onClick={onSignup}>Signup Here</button>
+      <Link href="/login">Visit Login Page</Link>
     </div>
   )
 }
